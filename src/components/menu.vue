6bc9e38ee353638496c5d70069a962452bc0c16e
DavidMayorgaS08 <david08102@hotmail.com>
2024-06-16 13:56:56 -0500

    cerrar sesion

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="menu">
       <div class="hamburger" @click="menu()">
-        <input class="checkbox" type="checkbox" v-model="isChecked"/>
+        <input class="checkbox" type="checkbox" v-model="isChecked" />
         <svg fill="none" viewBox="0 0 50 50" height="50" width="50">
           <path
             class="lineTop line"
@@ -29,16 +29,52 @@
       </div>
     </div>
     <div :class="{ cont_menu: true, 'menu-open': menuOpen }">
-      <router-link to="/cliente"><button class="btn">Clientes</button></router-link>
-      <router-link to="/ingreso"><button class="btn" @click="ingreso()">Ingresos</button></router-link>
-      <router-link to="/inventario"><button class="btn" @click="inventario()">Inventarios</button></router-link>
-      <router-link to="/mantenimiento"><button class="btn" @click="mantenimiento()">Mantenimientos</button></router-link>
-      <router-link to="/maquina"><button class="btn" @click="maquina()">Maquinas</button></router-link>
-      <router-link to="/pago"><button class="btn" @click="pago()">Pagos</button></router-link>
-      <router-link to="/plan"><button class="btn" @click="plan()">Planes</button></router-link>
-      <router-link to="/sede"><button class="btn" @click="sede()">Sedes</button></router-link>
-      <router-link to="/usuarios"><button class="btn" @click="usuario()">Usuarios</button></router-link>
-      <router-link to="/venta"><button class="btn" @click="venta()">Ventas</button></router-link>
+      <router-link to="/cliente"
+        ><button class="btn">Clientes</button></router-link
+      >
+      <router-link to="/ingreso"
+        ><button class="btn" @click="ingreso()">Ingresos</button></router-link
+      >
+      <router-link to="/inventario"
+        ><button class="btn" @click="inventario()">
+          Inventarios
+        </button></router-link
+      >
+      <router-link to="/mantenimiento"
+        ><button class="btn" @click="mantenimiento()">
+          Mantenimientos
+        </button></router-link
+      >
+      <router-link to="/maquina"
+        ><button class="btn" @click="maquina()">Maquinas</button></router-link
+      >
+      <router-link to="/pago"
+        ><button class="btn" @click="pago()">Pagos</button></router-link
+      >
+      <router-link to="/plan"
+        ><button class="btn" @click="plan()">Planes</button></router-link
+      >
+      <router-link to="/sede"
+        ><button class="btn" @click="sede()">Sedes</button></router-link
+      >
+      <router-link to="/usuarios"
+        ><button class="btn" @click="usuario()">Usuarios</button></router-link
+      >
+      <router-link to="/venta"
+        ><button class="btn" @click="venta()">Ventas</button></router-link
+      >
+    </div>
+    <div class="cont_btn_salir">
+      <button class="Btn" @click="cerrarSesion()">
+        <div class="sign">
+          <svg viewBox="0 0 512 512">
+            <path
+              d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"
+            ></path>
+          </svg>
+        </div>
+        <div class="text">Cerrar sesión</div>
+      </button>
     </div>
     <div class="cont_info">
       <router-view />
@@ -165,6 +201,11 @@ const venta = async () => {
   } catch (error) {
     console.error(error);
   }
+};
+
+const cerrarSesion = () => {
+  localStorage.removeItem("usuario");
+  window.location.href = "/";
 };
 </script>
   <style scoped>
@@ -316,11 +357,85 @@ const venta = async () => {
   background-size: 100% 30px;
 }
 
-.cont_info{
+.cont_info {
   display: flex;
   align-items: center;
   height: 100vh;
   width: 100%;
+}
+
+.cont_btn_salir {
+  position: absolute;
+  z-index: 100;
+  bottom: 10px;
+  right: 10px;
+}
+
+.Btn {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 50px;
+  height: 50px;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition-duration: .3s;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.199);
+  background-color: rgb(255, 65, 65);
+}
+
+/* plus sign */
+.sign {
+  width: 100%;
+  transition-duration: .3s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.sign svg {
+  width: 17px;
+}
+
+.sign svg path {
+  fill: white;
+}
+/* text */
+.text {
+  position: absolute;
+  right: 0%;
+  width: 0%;
+  opacity: 0;
+  color: white;
+  font-size: 1.2em;
+  font-weight: 600;
+  transition-duration: .3s;
+}
+/* hover effect on button width */
+.Btn:hover {
+  width: 145px;
+  border-radius: 40px;
+  transition-duration: .3s;
+}
+
+.Btn:hover .sign {
+  width: 30%;
+  transition-duration: .3s;
+  padding-left: 20px;
+}
+/* hover effect button's text */
+.Btn:hover .text {
+  opacity: 1;
+  width: 70%;
+  transition-duration: .3s;
+  padding-right: 10px;
+}
+/* button click effect*/
+.Btn:active {
+  transform: translate(2px ,2px);
 }
 </style>
   
